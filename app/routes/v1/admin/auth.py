79c94aa34d0c7f11fe72547e4d41d7ab3b6ad72b
tjdev <thetejasjagdale@gmail.com>
2026-01-05 08:02:26 +0530

    feat(auth):admin auth

--- app/routes/v1/admin/auth.py
+++ app/routes/v1/admin/auth.py
@@ -1,12 +1,22 @@
 from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 
+from app.schemas.auth.admin import AdminSignUpRequest
 from app.types.db import DBSession
-
+from app.services.auth.admin import signup_admin
 
 # configure the router
 router = APIRouter()
 
 
-
 @router.post("/signup")
-async def admin_signup(db:DBSession,payload:payload)
+async def admin_signup(db: DBSession, payload: AdminSignUpRequest):
+    # get the admin
+    admin = await signup_admin(db=db, payload=payload)
+
+    # return the admin
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"success": "True", "message": "Admin Created Successfully"},
+    )
