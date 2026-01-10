bf076d5d7a817e9e65879f3ea1559a9d94d0ac63
tjdev <thetejasjagdale@gmail.com>
2026-01-10 16:05:57 +0530

    feat(company onboarding):added the company onboarding

--- app/types/auth.py
+++ app/types/auth.py
@@ -1,5 +1,11 @@
 from uuid import UUID as _UUID
 
+from pydantic import BaseModel
 
 # Return the type
 TypeUUID = _UUID
+
+
+# User Schema for the dependency
+class UserData(BaseModel):
+    user_id: str
