873ff9d15ad3b327029c96a325521fc890f1b1e7
tjdev <thetejasjagdale@gmail.com>
2026-01-11 11:42:18 +0530

    feat(services):Added services schemas

---
+++ app/schemas/services/service.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+#  Enum for the environment types
+class EnvironmentType(str, Enum):
+    DEVELOPMENT = "development"
+    STAGING = "staging"
+    PRODUCTION = "production"
+
+
+# Schema to accept the service details
+class ServiceRegisterRequest(BaseModel):
+    company_id: str
+    name: str
+    base_url: str
+    scope_path: str | None = None
+    environment: EnvironmentType
+
+
+# Schema to update the service details
+class ServiceUpdateRequest(BaseModel):
+    name: str | None = None
+    base_url: str | None = None
+    scope_path: str | None = None
+    environment: EnvironmentType | None = None
+    is_active: bool | None = None
+
+
+# Schema to delte the service
+class ServiceDeleteRequest(BaseModel):
+    service_id: str
+    company_id: str
